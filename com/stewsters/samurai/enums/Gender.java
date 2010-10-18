9f869e67cfec835aa37eb673414feb84e63583a8
Adrian Moore <adrianmoore@adrianmoore.sparknet.lan>
2010-10-18 13:11:17 -0500

    Deleting

--- com/stewsters/samurai/enums/Gender.java
+++
@@ -1,6 +0,0 @@
-package com.stewsters.samurai.enums;
-
-public enum Gender
-{
-	MALE,FEMALE;
-}
--- com/stewsters/samurai/weapons/Armor.java
+++
@@ -1,8 +0,0 @@
-package com.stewsters.samurai.weapons;
-
-public class Armor
-{
-	public int defense;
-	public int weight;
-	
-}
